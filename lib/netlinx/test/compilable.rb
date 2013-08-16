4e8dec68dc08e5eb564801eac5b7ed91795df25e
Alex McLain <alex@alexmclain.com>
2013-08-15 20:53:27 -0700

    Added :nodoc: to Compilable methods.

--- lib/netlinx/test/compilable.rb
+++ lib/netlinx/test/compilable.rb
@@ -3,35 +3,42 @@ module NetLinx
     # An object that can be compiled with NetLinxCompiler.
     module Compilable
       
+      # :nodoc:
       def test_responds_to_compiler_target_files
         assert_respond_to @object, :compiler_target_files
       end
       
+      # :nodoc:
       def test_responds_to_compiler_include_paths
         assert_respond_to @object, :compiler_include_paths
       end
       
+      # :nodoc:
       def test_responds_to_compiler_library_paths
         assert_respond_to @object, :compiler_library_paths
       end
       
+      # :nodoc:
       def test_responds_to_compiler_module_paths
         assert_respond_to @object, :compiler_module_paths
       end
       
-      
+      # :nodoc:
       def test_compiler_target_files_is_an_array
         assert @object.compiler_target_files.is_a? Array
       end
-        
+      
+      # :nodoc:
       def test_compiler_include_paths_is_an_array
         assert @object.compiler_include_paths.is_a? Array
       end
-        
+      
+      # :nodoc:  
       def test_compiler_library_paths_is_an_array
         assert @object.compiler_library_paths.is_a? Array
       end
-        
+      
+      # :nodoc:
       def test_compiler_module_paths_is_an_array
         assert @object.compiler_module_paths.is_a? Array
       end

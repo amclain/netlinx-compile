05bf271b72aae00687df1e5a6a26a3b1aa1a7fdf
Alex McLain <alex@alexmclain.com>
2013-08-16 21:40:03 -0700

    Renamed compile Extendable interface to Invokable.

---
+++ lib/test/netlinx/compile/invokable.rb
@@ -0,0 +1,14 @@
+module Test
+  module NetLinx
+    module Compile
+      # Interface tests for an object that provides a means to
+      # invoke the compiler on a Compilable file extension.
+      # See ExtensionDiscovery.
+      class Invokable
+        def test_responds_to_invoke_compile
+          assert_respond_to @object, :invoke_compile
+        end
+      end
+    end
+  end
+end

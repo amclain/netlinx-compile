fb22246245849d857ffa2b91a15b07e9bc0a3128
Alex McLain <alex@alexmclain.com>
2013-08-18 21:57:03 -0700

    Implemented AXS extension handler.

--- lib/netlinx/compile/extension/axs.rb
+++ lib/netlinx/compile/extension/axs.rb
@@ -1,9 +1,23 @@
+require 'netlinx/compiler'
+require 'ostruct'
+
 module NetLinx
   module Compile
     module Extension
       # Instructs netlinx-compile on how to process .axs NetLinx source code files.
       class AXS
-        def invoke_compile
+        def invoke_compile(**kvargs)
+          target = kvargs.fetch(:target, nil)
+          raise ArgumentError, "Invalid target: #{target}." unless target
+          
+          compilable = OpenStruct.new \
+            compiler_target_files:  [target],
+            compiler_include_paths: [],
+            compiler_module_paths:  [],
+            compiler_library_paths: []
+          
+          @compiler = NetLinx::Compiler.new
+          @compiler.compile compilable
         end
       end
     end
--- test/unit/compile/extension/axs_test.rb
+++ test/unit/compile/extension/axs_test.rb
@@ -6,6 +6,7 @@ describe NetLinx::Compile::Extension::AXS do
   include Test::NetLinx::Compile::Invokable
   
   before do
+    @path = 'test\unit\workspace\extension\axs'
     @axs = @object = NetLinx::Compile::Extension::AXS.new
   end
   
@@ -14,6 +15,25 @@ describe NetLinx::Compile::Extension::AXS do
   end
   
   it "invokes the compiler for a file with the .axs extension" do
-    skip
+    source = File.expand_path 'axs-file.axs', @path
+    destination = File.expand_path 'axs-file.tkn', @path
+    File.delete destination if File.exists? destination
+    
+    @axs.invoke_compile target: source
+    
+    File.exists?(destination).must_equal true
+  end
+  
+  it "raises ArgumentError if the target is not specified" do
+    Proc.new {
+      @axs.invoke_compile
+    }.must_raise ArgumentError
+  end
+  
+  it "returns a compiler result" do
+    source = File.expand_path 'axs-file.axs', @path
+    
+    @axs.invoke_compile(target: source).first
+      .is_a?(NetLinx::CompilerResult).must_equal true
   end
 end

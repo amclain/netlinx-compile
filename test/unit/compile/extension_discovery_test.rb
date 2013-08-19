ce79bb69d19ee48b79e0ec77723a78342c4bd02a
Alex McLain <alex@alexmclain.com>
2013-08-18 21:07:14 -0700

    Implemented ExtensionDiscovery.

--- test/unit/compile/extension_discovery_test.rb
+++ test/unit/compile/extension_discovery_test.rb
@@ -19,16 +19,28 @@ describe NetLinx::Compile::ExtensionDiscovery do
     # allowing netlinx-compile to dynamically figure out how to process
     # different file types.
     
-    assert_equal true, @extension_discovery.load_handler('.axs')
+    @extension_discovery.load_handler('.axs').must_equal true
   end
   
   it "works with or without a leading dot in the file extension" do
-    assert_equal true, @extension_discovery.load_handler('.axs')
-    assert_equal true, @extension_discovery.load_handler('axs')
+    @extension_discovery.load_handler('.axs').must_equal true
+    @extension_discovery.load_handler('axs').must_equal true
   end
   
   it "works if the file name/path is entered instead of the extension" do
-    assert_equal true, @extension_discovery.load_handler('my_file.axs')
-    assert_equal true, @extension_discovery.load_handler('c:/path/to/my_file.axs')
+    @extension_discovery.load_handler('my_file.axs').must_equal true
+    @extension_discovery.load_handler('c:/path/to/my_file.axs').must_equal true
+  end
+  
+  it "raises LoadError if a library is not available for the extension specified" do
+    Proc.new {
+      @extension_discovery.load_handler('.invalid_extension')
+    }.must_raise LoadError
+  end
+  
+  it "raises ArgumentError if invalid data is given" do
+    Proc.new {
+      @extension_discovery.load_handler('')
+    }.must_raise ArgumentError
   end
 end

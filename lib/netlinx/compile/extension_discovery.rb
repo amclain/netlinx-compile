f641d8b0aa6bafdf82065ef9618ebd357eeb5103
Alex McLain <alex@alexmclain.com>
2013-08-18 21:07:14 -0700

    Implemented ExtensionDiscovery.

--- lib/netlinx/compile/extension_discovery.rb
+++ lib/netlinx/compile/extension_discovery.rb
@@ -6,7 +6,17 @@ module NetLinx
       # Loads (requires) a library based on the given file extension.
       # The extension can be provided with or without the leading dot.
       # Returns true if a library was loaded.
+      # Raises LoadError if no handler library is found.
       def load_handler(extension)
+        ext = extension.scan(/(\w+)$/).first
+        
+        # No value was matched.
+        raise ArgumentError unless ext
+        
+        ext = ext.first
+        
+        require "netlinx/compile/extension/#{ext}"
+        true
       end
     end
   end

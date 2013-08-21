415a1787d3f5b072af4fa808c3bb97d264323b86
Alex McLain <alex@alexmclain.com>
2013-08-20 22:16:07 -0700

    SourceFile implements Compilable.

--- lib/netlinx/source_file.rb
+++ lib/netlinx/source_file.rb
@@ -1,4 +1,27 @@
 module NetLinx
   class SourceFile
+    
+    def initialize(**kvargs)
+      @compiler_target_files  = kvargs.fetch :compiler_target_files,  []
+      @compiler_include_paths = kvargs.fetch :compiler_include_paths, []
+      @compiler_module_paths  = kvargs.fetch :compiler_module_paths,  []
+    end
+    
+    def compiler_target_files
+      @compiler_target_files
+    end
+    
+    def compiler_include_paths
+      @compiler_include_paths
+    end
+    
+    def compiler_module_paths
+      @compiler_module_paths
+    end
+    
+    def compiler_library_paths
+      []
+    end
+    
   end
 end
--- test/unit/source_file_test.rb
+++ test/unit/source_file_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 require 'netlinx/source_file'
+require 'test/netlinx/compilable'
 
 describe NetLinx::SourceFile do
+  include Test::NetLinx::Compilable
+  
+  before do
+    @source_file = @object = NetLinx::SourceFile.new
+  end
+  
+  after do
+    @source_file = @object = nil
+  end
+  
   it "does something" do
     skip
   end

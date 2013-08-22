a25ec074dc06eac72b086d0488bbf9ed0671783a
Alex McLain <alex@alexmclain.com>
2013-08-21 23:14:50 -0700

    Wrote SourceFile include and module path discovery tests.

--- lib/netlinx/source_file.rb
+++ lib/netlinx/source_file.rb
@@ -1,13 +1,21 @@
 module NetLinx
   class SourceFile
     
+    # Include directive regex:
+    # (?i)^\s*(\#include)\s+'([\w\-]+)'
+    
+    # Define_Module regex:
+    # (?i)^\s*(define_module)\s+'([\w\-]+)'
+    
     def initialize(**kvargs)
-      @compiler_target_files  =
-        kvargs.fetch :compiler_target_files, nil ||
-        [ (kvargs.fetch :file, []) ]
-      
+      @compiler_target_files  = [ (kvargs.fetch :file, nil) ]
       @compiler_include_paths = kvargs.fetch :compiler_include_paths, []
       @compiler_module_paths  = kvargs.fetch :compiler_module_paths,  []
+      
+      # Scan file for additional include paths.
+      
+      # Scan file for additional module paths.
+      
     end
     
     def compiler_target_files

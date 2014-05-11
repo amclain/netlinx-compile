8d0c31fe2aef48839c0948630bec54a8c4d366ed
Alex McLain <alex@alexmclain.com>
2014-05-11 11:28:37 -0700

    Converted extension_discovery_spec to rspec.

--- spec/compile/extension_discovery_spec.rb
+++ spec/compile/extension_discovery_spec.rb
@@ -10,41 +10,32 @@ describe NetLinx::Compile::ExtensionDiscovery do
   # extension, it is more efficient to take some of the discovery process on
   # faith because the AXS test will show if it's working correctly.
   
-  before do
-    @extension_discovery = @object = NetLinx::Compile::ExtensionDiscovery
-  end
+  subject { NetLinx::Compile::ExtensionDiscovery }
   
-  after do
-    @extension_discovery = @object = nil
-  end
+  
+  it { should respond_to :discover }
   
   it "is a singleton" do
-    Proc.new {
-      NetLinx::Compile::ExtensionDiscovery.new
-    }.must_raise NoMethodError
-  end
-  
-  it "responds to discover" do
-    assert_respond_to @extension_discovery, :discover
+    expect { NetLinx::Compile::ExtensionDiscovery.new }.to raise_error NoMethodError
   end
   
   it "runs discover without raising an exception" do
-    @extension_discovery.discover
-    assert true
+    subject.discover
   end
   
   it "returns a handler for a given file or file extension" do
-    @extension_discovery.discover
-    @extension_discovery.get_handler('axs')
+    subject.discover
+    subject.get_handler('axs')
       .is_a?(NetLinx::Compile::ExtensionHandler)
-      .must_equal true
+      .should eq true
   end
   
   it "returns an array of workspace handlers" do
+    pending
     # NOTE: netlinx-workspace gem must be installed for this to pass.
     # TODO: Stub it.
-    @extension_discovery.discover
-    @extension_discovery.workspace_extensions.include?('apw').must_equal true
+    subject.discover
+    subject.workspace_extensions.include?('apw').should eq true
   end
   
 end

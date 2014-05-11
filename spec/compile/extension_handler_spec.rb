5fe7369ad46a060b540fd158dc052a12f7c2e553
Alex McLain <alex@alexmclain.com>
2014-05-11 11:17:36 -0700

    Converted extension_handler_spec to rspec.

--- spec/compile/extension_handler_spec.rb
+++ spec/compile/extension_handler_spec.rb
@@ -2,57 +2,47 @@ require 'netlinx/compile/extension_handler'
 
 describe NetLinx::Compile::ExtensionHandler do
   
-  before do
-    @extension_handler = @object = NetLinx::Compile::ExtensionHandler.new
-  end
-  
-  after do
-    @extension_handler = @object = nil
-  end
-  
   it "exposes a set of extensions that it can handle" do
-    assert_respond_to @extension_handler, :extensions
+    subject.should respond_to :extensions
     
     # Implements array methods.
-    @extension_handler.extensions.count.must_equal 0
-    @extension_handler.extensions.first.must_equal nil
+    subject.extensions.count.should be 0
+    subject.extensions.first.should be nil
   end
   
   it "exposes a set of extensions it usurps" do
     # For example, third-party workspace extensions would
     # probably usurp the .apw workspace extension.
     
-    assert_respond_to @extension_handler, :usurps
+    subject.should respond_to :usurps
     
     # Implements array methods.
-    @extension_handler.extensions.count.must_equal 0
-    @extension_handler.extensions.first.must_equal nil
+    subject.extensions.count.should be 0
+    subject.extensions.first.should be nil
   end
   
   it "exposes if it is a workspace" do
-    @extension_handler.is_a_workspace?.must_equal false
+    subject.is_a_workspace?.should be false
   end
   
   it "can check to see if it includes a given extension" do
-    assert_respond_to @extension_handler, :include?
+    subject.should respond_to :include?
     
-    @extension_handler.extensions << 'apw'
-    @extension_handler.include?('.apw').must_equal true
-    @extension_handler.include?('.axs').must_equal false
+    subject.extensions << 'apw'
+    subject.include?('.apw').should be true
+    subject.include?('.axs').should be false
     
-    Proc.new {
-      @extension_handler.include?('invalid extension')
-    }.must_raise ArgumentError
+    expect { subject.include?('invalid extension') }.to raise_error ArgumentError
   end
     
   it "exposes a class to handle its extensions" do
-    assert_respond_to @extension_handler, :handler_class
+    subject.should respond_to :handler_class
   end
   
   it "defines << operator as an alias to add an extension" do
-    assert_respond_to @extension_handler, :<<
+    subject.should respond_to :<<
     
-    @extension_handler << 'apw'
-    @extension_handler.extensions.include?('apw').must_equal true
+    subject << 'apw'
+    subject.extensions.include?('apw').should be true
   end
 end

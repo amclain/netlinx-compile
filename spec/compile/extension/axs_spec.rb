a371fc0c60744cb178c94e91709cc5b2fbe88120
Alex McLain <alex@alexmclain.com>
2014-05-11 11:35:11 -0700

    Converted axs_spec to rspec.

--- spec/compile/extension/axs_spec.rb
+++ spec/compile/extension/axs_spec.rb
@@ -2,21 +2,19 @@ require 'netlinx/compile/extension/axs'
 require 'test/netlinx/compile/discoverable'
 
 describe NetLinx::Compile::Extension::AXS do
-  include Test::NetLinx::Compile::Discoverable
   
-  before do
-    @path = 'test\unit\workspace\extension\axs'
-    @axs = @object = NetLinx::Compile::Extension::AXS
-  end
+  subject { NetLinx::Compile::Extension::AXS }
   
-  after do
-    @axs = @object = nil
-  end
+  let(:path) { 'test/unit/workspace/extension/axs' }
+  
+  
+  include_examples "discoverable"
   
   it "handles .axs and .axi NetLinx source code files" do
-    @axs.get_handler.extensions.include?('axs').must_equal true
-    @axs.get_handler.extensions.include?('axi').must_equal true
+    subject.get_handler.extensions.include?('axs').should eq true
+    subject.get_handler.extensions.include?('axi').should eq true
     
-    @axs.get_handler.handler_class.must_equal NetLinx::SourceFile
+    subject.get_handler.handler_class.should eq NetLinx::SourceFile
   end
+  
 end

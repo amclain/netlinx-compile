1449398dd716fc08a43ffcd496feb3ffb2c674d3
Alex McLain <alex@alexmclain.com>
2014-05-11 11:41:40 -0700

    Converted compiler_result_spec to rspec.

--- lib/test/netlinx/compilable.rb
+++ lib/test/netlinx/compilable.rb
@@ -1,48 +1,13 @@
-module Test
-  module NetLinx
-    # Interface tests for an object that can be compiled with NetLinx::Compiler.
-    module Compilable
-      
-      # :nodoc:
-      def test_responds_to_compiler_target_files
-        assert_respond_to @object, :compiler_target_files
-      end
-      
-      # :nodoc:
-      def test_responds_to_compiler_include_paths
-        assert_respond_to @object, :compiler_include_paths
-      end
-      
-      # :nodoc:
-      def test_responds_to_compiler_library_paths
-        assert_respond_to @object, :compiler_library_paths
-      end
-      
-      # :nodoc:
-      def test_responds_to_compiler_module_paths
-        assert_respond_to @object, :compiler_module_paths
-      end
-      
-      # :nodoc:
-      def test_compiler_target_files_is_an_array
-        assert @object.compiler_target_files.is_a? Array
-      end
-      
-      # :nodoc:
-      def test_compiler_include_paths_is_an_array
-        assert @object.compiler_include_paths.is_a? Array
-      end
-      
-      # :nodoc:  
-      def test_compiler_library_paths_is_an_array
-        assert @object.compiler_library_paths.is_a? Array
-      end
-      
-      # :nodoc:
-      def test_compiler_module_paths_is_an_array
-        assert @object.compiler_module_paths.is_a? Array
-      end
-      
-    end
-  end
+shared_examples "compilable" do
+  
+  it { should respond_to :compiler_target_files  }
+  it { should respond_to :compiler_include_paths }
+  it { should respond_to :compiler_library_paths }
+  it { should respond_to :compiler_module_paths  }
+  
+  its(:compiler_target_files)  { should be_an Array }
+  its(:compiler_include_paths) { should be_an Array }
+  its(:compiler_library_paths) { should be_an Array }
+  its(:compiler_module_paths)  { should be_an Array }
+  
 end
